63fb83ceb8b9cbad78af419a3c19e3fa02d069b9
Di5to <di5tortion@yahoo.com>
2020-05-14 21:21:21 -0400

    code for pre/post-NER-processing CORD-19 abstracts and their output files

--- Multi-BioNER/model/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = "Liyuan Liu and Frank Xu"
-__credits__ = ["Liyuan Liu", "Frank Xu", "Jingbo Shang"]
-
-__license__ = "Apache License 2.0"
-__maintainer__ = "Liyuan Liu"
-__email__ = "llychinalz@gmail.com"
